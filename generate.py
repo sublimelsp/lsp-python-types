52fdb34f194ddf97300837372d39be9445fa064f
Rafal Chlodnicki <rchl2k@gmail.com>
2022-09-16 23:29:10 +0200

    remove generate_requests

--- generate.py
+++ generate.py
@@ -3,7 +3,6 @@ import json
 
 from lsp_schema import MetaModel
 from utils.generate_enumerations import generate_enumerations
-# from utils.generate_requests import generate_requests
 from utils.generate_structures import generate_structures
 from utils.generate_type_aliases import generate_type_aliases
 from utils.helpers import get_new_literal_structures
@@ -24,7 +23,6 @@ def generate() -> None:
             "DocumentUri = str",
             "Uint = int",
             "RegExp = str",
-            # generate_requests(lsp_json['requests'])
         ])
 
         content += '\n\n\n'
--- utils/generate_requests.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from lsp_schema import Request
-
-
-def generate_requests(requests: List[Request]) -> str:
-    request_names = [f"'{request['method']}'" for request in requests]
-    result = f"Methods = Literal[{', '.join(request_names)}]"
-    return result
