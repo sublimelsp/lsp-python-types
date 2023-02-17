075128da02f899f5aa73f43f83326514b7c4a913
Предраг Николић <idmpepe@gmail.com>
2023-02-17 12:53:11 +0100

    make it work

--- hello.py
+++ hello.py
@@ -1,21 +1,111 @@
 import asyncio
 import json
 import os
+from typing import Any, Dict, List, Union, Optional
 from lsp_request_context import RequestContext
-from lsp_requests import LspNotification, LspRequest, Response
+from lsp_requests import LspNotification, LspRequest
+from lsp_types import ErrorCodes
 
 
-
+StringDict = Dict[str, Any]
+PayloadLike = Union[List[StringDict], StringDict, None]
 CONTENT_LENGTH = 'Content-Length: '
+ENCODING = "utf-8"
+
+
+class Error(Exception):
+
+    def __init__(self, code: ErrorCodes, message: str) -> None:
+        super().__init__(message)
+        self.code = code
+
+    def to_lsp(self) -> StringDict:
+        return {"code": self.code, "message": super().__str__()}
+
+    @classmethod
+    def from_lsp(cls, d: StringDict) -> 'Error':
+        return Error(d["code"], d["message"])
+
+    def __str__(self) -> str:
+        return f"{super().__str__()} ({self.code})"
+
+
+def make_response(request_id: Any, params: PayloadLike) -> StringDict:
+    return {"jsonrpc": "2.0", "id": request_id, "result": params}
+
+def make_error_response(request_id: Any, err: Error) -> StringDict:
+    return {"jsonrpc": "2.0", "id": request_id, "error": err.to_lsp()}
+
+def make_notification(method: str, params: PayloadLike) -> StringDict:
+    return {"jsonrpc": "2.0", "method": method, "params": params}
+
+def make_request(method: str, request_id: Any, params: PayloadLike) -> StringDict:
+    return {"jsonrpc": "2.0", "method": method, "id": request_id, "params": params}
+
+
+class StopLoopException(Exception):
+    pass
+
+def create_message(payload: PayloadLike) :
+    body = json.dumps(
+        payload,
+        check_circular=False,
+        ensure_ascii=False,
+        separators=(",", ":")).encode(ENCODING)
+    return (
+        f"Content-Length: {len(body)}\r\n".encode(ENCODING),
+        "Content-Type: application/vscode-jsonrpc; charset=utf-8\r\n\r\n".encode(ENCODING),
+        body
+    )
+
+class MessageType:
+    error = 1
+    warning = 2
+    info = 3
+    log = 4
+
+class Request():
+    global_id = 1
+    def __init__(self) -> None:
+        self.id = self.global_id
+        self.global_id +=1
+        self.cv = asyncio.Condition()
+        self.result = None  # type: PayloadLike
+        self.error = None  # type: Optional[Error]
+
+    async def on_result(self, params: PayloadLike) -> None:
+        self.result = params
+        async with self.cv:
+            self.cv.notify()
+
+    async def on_error(self, err: Error) -> None:
+        self.error = err
+        async with self.cv:
+            self.cv.notify()
+
+
+def content_length(line: bytes) -> Optional[int]:
+    if line.startswith(b'Content-Length: '):
+        _, value = line.split(b'Content-Length: ')
+        value = value.strip()
+        try:
+            return int(value)
+        except ValueError:
+            raise ValueError("Invalid Content-Length header: {}".format(value))
+    return None
+
 class Server():
     def __init__(self, cmd: str) -> None:
-        self.process = None
-        self.cmd = cmd
-        self.request_id = 1
         self.request = LspRequest(self.send_request)
         self.notify = LspNotification(self.send_notification)
-        self.on_request_callbacks = {}
-        self.on_notification_callbacks = {}
+
+        self.cmd = cmd
+        self.process = None
+        self._received_shutdown = False
+
+        self._response_handlers: Dict[Any, Request] = {}
+        self.on_request_handlers = {}
+        self.on_notification_handlers = {}
 
     async def start(self):
         self.process = await asyncio.create_subprocess_shell(
@@ -23,43 +113,93 @@ class Server():
             stdout=asyncio.subprocess.PIPE,
             stdin=asyncio.subprocess.PIPE,
         )
+        asyncio.get_event_loop().create_task(self.run_forever())
 
     def stop(self):
         if self.process:
             self.process.kill()
 
+    def _log(self, message: str) -> None:
+        self.send_notification("window/logMessage",
+                     {"type": MessageType.info, "message": message})
+
+    async def run_forever(self) -> bool:
+        try:
+            while self.process and self.process.stdout and not self.process.stdout.at_eof():
+                line = await self.process.stdout.readline()
+                if not line:
+                    continue
+                try:
+                    num_bytes = content_length(line)
+                except ValueError:
+                    continue
+                if num_bytes is None:
+                    continue
+                while line and line.strip():
+                    line = await self.process.stdout.readline()
+                if not line:
+                    continue
+                body = await self.process.stdout.readexactly(num_bytes)
+                asyncio.get_event_loop().create_task(self._handle_body(body))
+        except(BrokenPipeError, ConnectionResetError, StopLoopException):
+            pass
+        return self._received_shutdown
+
+    async def _handle_body(self, body: bytes) -> None:
+        try:
+            await self._receive_payload(json.loads(body))
+        except IOError as ex:
+            self._log(f"malformed {ENCODING}: {ex}")
+        except UnicodeDecodeError as ex:
+            self._log(f"malformed {ENCODING}: {ex}")
+        except json.JSONDecodeError as ex:
+            self._log(f"malformed JSON: {ex}")
+
+    async def _receive_payload(self, payload: StringDict) -> None:
+        try:
+            if "method" in payload:
+                if "id" in payload:
+                    await self._request_handler(payload)
+                else:
+                    await self._notification_handler(payload)
+            elif "id" in payload:
+                await self._response_handler(payload)
+            else:
+                self._log(f"Unknown payload type: {payload}")
+        except Exception as err:
+            self._log(f"Error handling server payload: {err}")
+
     def send_notification(self, method: str, params: dict):
-        self.request_id = 1
-        content = json.dumps({
-            "jsonrpc": "2.0",
-            "method": method,
-            "params": params
-        })
-        header = CONTENT_LENGTH + str(len(content))
-        msg = header + '\r\n\r\n' + content
-        if not self.process or not self.process.stdin:
-            return
-        self.process.stdin.write(msg.encode('UTF-8'))
+        asyncio.get_event_loop().create_task(self._send_payload(
+            make_notification(method, params)))
+
+    def send_response(self, request_id: Any, params: PayloadLike) -> None:
+        asyncio.get_event_loop().create_task(self._send_payload(
+            make_response(request_id, params)))
+
+    def send_error_response(self, request_id: Any, err: Error) -> None:
+        asyncio.get_event_loop().create_task(self._send_payload(
+            make_error_response(request_id, err)))
 
     async def send_request(self, method: str, params: dict):
-        self.request_id = 1
-        content = json.dumps({
-            "jsonrpc": "2.0",
-            "id": self.request_id,
-            "method": method,
-            "params": params
-        })
-        header = CONTENT_LENGTH + str(len(content))
-        msg = header + '\r\n\r\n' + content
+        request = Request()
+        self._response_handlers[request.id] = request
+        async with request.cv:
+            await self._send_payload(make_request(method, request.id, params))
+            await request.cv.wait()
+        if isinstance(request.error, Error):
+            raise request.error
+        return request.result
+
+    async def _send_payload(self, payload: StringDict) -> None:
         if not self.process or not self.process.stdin:
             return
-        self.process.stdin.write(msg.encode('UTF-8'))
-        context = self.request_context()
-        response = await self._wait_response(self.request_id, context)
-        return response
+        msg = create_message(payload)
+        self.process.stdin.writelines(msg)
+        await self.process.stdin.drain()
 
     def on_request(self, method: str, cb):
-        self.on_request_callbacks[method] = cb
+        self.on_request_handlers[method] = cb
 
     def request_context(self) -> RequestContext:
         return {
@@ -70,53 +210,47 @@ class Server():
         }
 
     def on_notification(self, method: str, cb):
-        self.on_notification_callbacks[method] = cb
+        self.on_notification_handlers[method] = cb
 
-    async def _wait_response(self, id, context):
-        while True:
-            if not self.process:
-                return
-            header_data = await self.process.stdout.readline()
-            header = header_data.decode('UTF-8').rstrip()
-            if len(header) > len(CONTENT_LENGTH):
-                content_size = int(header[len(CONTENT_LENGTH):])
-                await self.process.stdout.readline()
-                response_data = await self.process.stdout.read(content_size)
-                response = response_data.decode('UTF-8').rstrip()
+    async def _response_handler(self, response: StringDict) -> None:
+        request = self._response_handlers.pop(response["id"])
+        if "result" in response and "error" not in response:
+            await request.on_result(response["result"])
+        elif "result" not in response and "error" in response:
+            await request.on_error(Error.from_lsp(response["error"]))
+        else:
+            await request.on_error(Error(ErrorCodes.InvalidRequest, ''))
 
-                response_obj = json.loads(response)
-                method = response_obj.get("method")
-                result = response_obj.get("result")
-                error = response_obj.get("error")
-                rpc_id = response_obj.get("id")
-                params = response_obj.get("params")
-                if rpc_id == id:
-                    return Response(response_obj, rpc_id, context)
-                if method:
-                    # request from server
-                    if rpc_id:
-                        cb = self.on_request_callbacks.get(method)
-                        if not cb:
-                            print('method not implemented', method)
-                            continue
-                        r = await cb(params)
-                        content = json.dumps({
-                            "id": rpc_id,
-                            "jsonrpc": "2.0",
-                            "result": r
-                        })
-                        header = CONTENT_LENGTH + str(len(content))
-                        msg = header + '\r\n\r\n' + content
-                        if not self.process or not self.process.stdin:
-                            continue
-                        self.process.stdin.write(msg.encode('UTF-8'))
-                        continue
-                    # notification from server
-                    cb = self.on_notification_callbacks.get(method)
-                    if not cb:
-                        print('notification not implemented', method)
-                        continue
-                    cb(params)
+    async def _request_handler(self, response: StringDict) -> None:
+        method = response.get("method", "")
+        params = response.get("params")
+        request_id = response.get("id")
+        handler = self.on_request_handlers.get(method)
+        if not handler:
+            self.send_error_response(request_id, Error(
+                    ErrorCodes.MethodNotFound, "method '{}' not handled on client.".format(method)))
+            return
+        try:
+            self.send_response(request_id, await handler(params))
+        except Error as ex:
+            self.send_error_response(request_id, ex)
+        except Exception as ex:
+            self.send_error_response(request_id, Error(ErrorCodes.InternalError, str(ex)))
+
+    async def _notification_handler(self, response: StringDict) -> None:
+        method = response.get("method", "")
+        params = response.get("params")
+        handler = self.on_notification_handlers.get(method)
+        if not handler:
+            self._log(f"unhandled {method}")
+            return
+        try:
+            await handler(params)
+        except asyncio.CancelledError:
+            return
+        except Exception as ex:
+            if not self._received_shutdown:
+                self.send_notification("window/logMessage", {"type": MessageType.error, "message": str(ex)})
 
 
 async def main():
@@ -138,8 +272,8 @@ async def main():
         'capabilities': {}
     }))
 
-    print('response', r.result)
-    print('context', r.context['server_name'])
+    print('response', r)
+    print('context', r)
 
     server.notify.did_open_text_document({
         'textDocument': {
