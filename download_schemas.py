3e77d943f774adff773870cc97b98e25afeea801
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-26 22:45:49 +0100

    fix schema download script

--- download_schemas.py
+++ download_schemas.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
+
 from pathlib import Path
 from urllib.request import urlopen
-
 
 REPO_URL = 'https://raw.githubusercontent.com/microsoft/vscode-languageserver-node'
 
 with urlopen(f'{REPO_URL}/main/protocol/metaModel.schema.json') as url:
-    Path('./lsprotocol/lsp.schema.json', 'w').write_text(url.read().decode('utf-8'))
+    Path('./lsprotocol/lsp.schema.json').write_text(url.read().decode('utf-8'))
 
 with urlopen(f'{REPO_URL}/main/protocol/metaModel.json'):
-    Path('./lsprotocol/lsp.json', 'w').write_text(url.read().decode('utf-8'))
+    Path('./lsprotocol/lsp.json').write_text(url.read().decode('utf-8'))
