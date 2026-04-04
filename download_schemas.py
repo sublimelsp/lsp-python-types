18ef625b46d8510d8380ce32dee02888d0b40721
Предраг Николић <idmpepe@gmail.com>
2026-04-04 19:38:47 +0200

    Generate 3.18.0 types (#34)
    I manually edited lsp.json to add diagnostic message union string MarkupContent

--- download_schemas.py
+++ download_schemas.py
@@ -5,10 +5,10 @@ from __future__ import annotations
 from pathlib import Path
 from urllib.request import urlopen
 
-REPO_URL = 'https://raw.githubusercontent.com/microsoft/vscode-languageserver-node'
+REPO_URL = 'https://raw.githubusercontent.com/microsoft/language-server-protocol'
 
-with urlopen(f'{REPO_URL}/main/protocol/metaModel.schema.json') as url:
+with urlopen(f'{REPO_URL}/refs/heads/gh-pages/_specifications/lsp/3.18/metaModel/metaModel.schema.json') as url:
     Path('./lsprotocol/lsp.schema.json').write_text(url.read().decode('utf-8'))
 
-with urlopen(f'{REPO_URL}/main/protocol/metaModel.json') as url:
+with urlopen(f'{REPO_URL}/refs/heads/gh-pages/_specifications/lsp/3.18/metaModel/metaModel.json') as url:
     Path('./lsprotocol/lsp.json').write_text(url.read().decode('utf-8'))
