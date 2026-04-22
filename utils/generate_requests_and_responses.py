8c62a6449a0807d32f4755e9b83514fda4c20b66
Rafał Chłodnicki <rchl2k@gmail.com>
2026-04-22 17:30:12 +0200

    Expose base type of enumeration (#39)

--- utils/generate_requests_and_responses.py
+++ utils/generate_requests_and_responses.py
@@ -5,10 +5,11 @@ from utils.helpers import format_type
 from utils.helpers import indentation
 
 if TYPE_CHECKING:
+    from lsp_schema import Enumeration
     from lsp_schema import Request
 
 
-def generate_requests_and_responses(requests: list[Request]) -> list[str]:
+def generate_requests_and_responses(requests: list[Request], enumerations: dict[str, Enumeration]) -> list[str]:
     client_request_names: list[str] = []
     server_request_names: list[str] = []
     client_response_names: list[str] = []
@@ -18,7 +19,7 @@ def generate_requests_and_responses(requests: list[Request]) -> list[str]:
     for request in requests:
         message_direction = request['messageDirection']
         # Requests
-        req_name, req_definition = generate_request(request)
+        req_name, req_definition = generate_request(request, enumerations)
         if message_direction == 'clientToServer':
             client_request_names.append(req_name)
         elif message_direction == 'serverToClient':
@@ -28,7 +29,7 @@ def generate_requests_and_responses(requests: list[Request]) -> list[str]:
             server_request_names.append(req_name)
         req_definitions.append(req_definition)
         # Responses
-        res_name, res_definition = generate_response(request)
+        res_name, res_definition = generate_response(request, enumerations)
         if message_direction == 'clientToServer':
             server_response_names.append(res_name)
         elif message_direction == 'serverToClient':
@@ -51,20 +52,20 @@ def generate_requests_and_responses(requests: list[Request]) -> list[str]:
     ]
 
 
-def generate_request(request: Request) -> tuple[str, str]:
+def generate_request(request: Request, enumerations: dict[str, Enumeration]) -> tuple[str, str]:
     method = request['method']
     params = request.get('params')
     name = request['typeName']
     definition = f'class {name}(TypedDict):\n'
     definition += f"{indentation}method: Literal['{method}']\n"
     if params:
-        definition += f'{indentation}params: {format_type(params, {"root_symbol_name": ""})}'
+        definition += f'{indentation}params: {format_type(params, {"enumerations": enumerations})}'
     else:
         definition += f'{indentation}params: None'
     return (name, definition)
 
 
-def generate_response(request: Request) -> tuple[str, str]:
+def generate_response(request: Request, enumerations: dict[str, Enumeration]) -> tuple[str, str]:
     method = request['method']
     result = request['result']
     params = request.get('params')
@@ -73,7 +74,7 @@ def generate_response(request: Request) -> tuple[str, str]:
     definition = f'class {name}(TypedDict):\n'
     definition += f"{indentation}method: Literal['{method}']\n"
     if request['messageDirection'] == 'serverToClient':
-        typ = format_type(params, {'root_symbol_name': ''}) if params else None
+        typ = format_type(params, {'enumerations': enumerations}) if params else None
         definition += f'{indentation}params: {typ}\n'
-    definition += f'{indentation}result: {format_type(result, {"root_symbol_name": ""})}'
+    definition += f'{indentation}result: {format_type(result, {"enumerations": enumerations})}'
     return (name, definition)
